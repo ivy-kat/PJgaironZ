67551917c66dad88f4c0006481d94790deadf80d
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:01:30 +0900

    微修正

--- Player.java
+++ Player.java
@@ -3,7 +3,7 @@ public class Player {
     public static String names[] = {"P1","P2"};
     public static int hands[] = new int[2];
     public static void chooseHands() {
-        for(int i=0;i<names.length;i++) {
+        for(int i = 0; i < names.length; i++) {
             hands[i]=Input.getHiddenInput(names[i]);
         }
     }
