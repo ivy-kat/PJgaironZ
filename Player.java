e967b830a0b2703efac421b5cc589b2730ccaec2
ivy-kat <kataoka@ivy.ac.jp>
2025-11-15 22:06:14 +0900

    メンバ変数の名称を変更

--- Player.java
+++ Player.java
@@ -1,10 +1,10 @@
 
 public class Player {
-    public static String names[] = {"P1","P2"};
-    public static int hands[] = new int[2];
+    public static String sNames[] = {"P1","P2"};
+    public static int sHands[] = new int[2];
     public static void chooseHands() {
-        for(int i = 0; i < names.length; i++) {
-            hands[i]=Input.getHiddenInput(names[i]);
+        for(int i = 0; i < sNames.length; i++) {
+            sHands[i]=Input.getHiddenInput(sNames[i]);
         }
     }
 }
